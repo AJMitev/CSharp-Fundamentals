d14aea41e5fd75d1c370662a3c266b7a5a7ce4d1
Aleksander Mitev <ajmitev@gmail.com>
2018-09-24 21:32:23 +0300

    Upload Solved Problems

---
+++ CSharp-Advanced/01.MultidimensionalArrays/Problem 07. Lego Blocks/Lego.cs
@@ -0,0 +1,74 @@
+﻿namespace Problem_07._Lego_Blocks
+{
+    using System;
+    using System.Linq;
+
+    public class Lego
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] firstJagged = new int[n][];
+            int[][] secondJagged = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                firstJagged[i] = Console.ReadLine()?
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                secondJagged[i] = Console.ReadLine()?
+                    .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            Console.WriteLine();
+
+            foreach (int[] numbers in secondJagged)
+            {
+                Array.Reverse(numbers);
+            }
+
+            var matrix = new int[n][];
+            var length = 0;
+            var isMatrix = true;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = firstJagged[i].Concat(secondJagged[i]).ToArray();
+                if (i == 0)
+                {
+                    length = matrix[i].Length;
+                }
+                else if (matrix[i].Length != length)
+                {
+                    isMatrix = false;
+                }
+
+            }
+
+            if (isMatrix)
+            {
+                foreach (var arr in matrix)
+                {
+                    var result = "[" + string.Join(", ", arr) + "]";
+                    Console.WriteLine(result);
+                }
+            }
+            else
+            {
+                int cels = 0;
+                foreach (var arr in matrix)
+                {
+                    cels += arr.Length;
+                }
+                Console.WriteLine($"The total number of cells is: {cels}");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/01.MultidimensionalArrays/Problem 09. Crossfire/Crossfire.cs
@@ -0,0 +1,92 @@
+﻿namespace Problem_09._Crossfire
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Crossfire
+    {
+        public static void Main()
+        {
+            int[] parameters = Console.ReadLine()?
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[][] matrix = new int[parameters[0]][];
+            int number = 1;
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                matrix[row] = new int[parameters[1]];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = number++;
+                }
+            }
+
+            string line = Console.ReadLine();
+
+            while (line != "Nuke it from orbit")
+            {
+                parameters = line?
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int row = parameters[0];
+                int col = parameters[1];
+                int radius = parameters[2];
+
+                if (row >= matrix.Length)
+                {
+                    line = Console.ReadLine();
+                    continue;
+                }
+
+                for (int rw = Math.Max(0, row - radius); rw < Math.Min(matrix.Length, row + radius+1); rw++)
+                {
+                    if (matrix[rw].Length > col)
+                    {
+                        matrix[rw][col] = -1;
+
+                    }
+                }
+
+                for (int cl = Math.Max(0, col - radius); cl < Math.Min(matrix[row].Length, row + radius+1); cl++)
+                {
+                    if (matrix[row].Length > cl)
+                    {
+                        matrix[row][cl] = -1;
+                    }
+                }
+
+                var queue = new Queue<int>();
+                for (int rw = 0; rw < matrix.Length; rw++)
+                {
+                    for (int i = 0; i < matrix[rw].Length; i++)
+                    {
+                        int currentSymbol = matrix[rw][i];
+                        if (!currentSymbol.Equals(-1))
+                        {
+                            queue.Enqueue(currentSymbol);
+                        }
+                    }
+                    matrix[rw] = new int[queue.Count];
+
+                    for (int i = 0; i < matrix[rw].Length; i++)
+                    {
+                        matrix[rw][i] = queue.Dequeue();
+                    }
+                }
+
+
+                line = Console.ReadLine();
+            }
+
+            foreach (int[] lineOfNumbers in matrix)
+            {
+                Console.WriteLine(string.Join(" ",lineOfNumbers));
+            }
+        }
+    }
+}
